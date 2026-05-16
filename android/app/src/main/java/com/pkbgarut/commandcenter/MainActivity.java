26977591f1af69b024abc424ad50e8fa3631da39
Faiz Sihab <arrumpurnamakarya@gmail.com>
2026-05-17 01:07:05 +0700

    Build: Konfigurasi Capacitor dan inisialisasi folder Android untuk pembuatan APK

---
+++ android/app/src/main/java/com/pkbgarut/commandcenter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pkbgarut.commandcenter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pkbgarut.commandcenter',
+  appName: 'Command Center PKB',
+  webDir: 'out' // <-- UBAH BAGIAN INI DARI 'public' MENJADI 'out'
+};
+
+export default config;
